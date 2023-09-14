573c247629a23f701761cf6196af899995589aeb
shifa-gani <124779666+shifa-gani@users.noreply.github.com>
2023-09-14 23:30:33 +0530

    sdsd

---
+++ Assets/Joystick Pack/Scripts/Playercontroller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class Playercontroller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
+    }
+}
